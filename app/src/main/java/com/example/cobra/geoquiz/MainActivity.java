cdfb58a60b30c3fad27b4c7abe362b642f6e8c57
maxim <maxim.semashko98@gmail.com>
2018-11-12 12:13:27 +0300

    Prevented multiple responses and broughted result of quiz

--- app/src/main/java/com/example/cobra/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/cobra/geoquiz/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.cobra.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static int CORRECT_ANSWERS=0;
 
     private Button mTrueButton;
     private Button mFalseButton;
     private TextView mQuestionText;
     private Button mNextButton;
+    private Button mPrevButton;
     private Question[] mQuestionBank = new Question[]{
         new Question(R.string.question_australia, true),
                 new Question(R.string.question_oceans, true),
@@ -28,10 +34,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
+
+        Log.d(TAG,"onCreate called");
         mTrueButton=findViewById(R.id.true_button);
         mFalseButton=findViewById(R.id.false_button);
         mNextButton=findViewById(R.id.next_button);
+        mPrevButton=findViewById(R.id.prev_button);
         mQuestionText=findViewById(R.id.question_text);
 
         updateQuestion();
@@ -39,16 +51,14 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               checkAnswer(mQuestionBank[mCurrentIndex].isAnswerTrue());
+               checkAnswer(true);
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP,0,0);
-                toast.show();
+               checkAnswer(false);
             }
         });
 
@@ -60,32 +70,95 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex=(mCurrentIndex-1)%mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
         //scrolling by textview
         mQuestionText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mCurrentIndex=(mCurrentIndex+1)%mQuestionBank.length;
                 updateQuestion();
             }
         });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart called");
+    }
+
+
     //method to scroll questions:
     private void updateQuestion(){
         int question=mQuestionBank[mCurrentIndex].getTexResId();
         mQuestionText.setText(question);
+        mTrueButton.setEnabled(true);
+        mFalseButton.setEnabled(true);
     }
 
     //method to check answer
     private void checkAnswer(boolean userPressedTrue){
         boolean isAnswerTrue= mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId = 0;
-        if(userPressedTrue==isAnswerTrue)
-            messageResId=R.string.correct_toast;
+        if(userPressedTrue==isAnswerTrue) {
+            messageResId = R.string.correct_toast;
+            CORRECT_ANSWERS++;
+        }
         else
             messageResId = R.string.incorrect_toast;
 
             Toast toast = Toast.makeText(MainActivity.this,messageResId,Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.TOP,0,0);
             toast.show();
+        mTrueButton.setEnabled(false);
+        mFalseButton.setEnabled(false);
 
+        if(mCurrentIndex==mQuestionBank.length-1){
+            mTrueButton.setEnabled(false);
+            mFalseButton.setEnabled(false);
+            mNextButton.setEnabled(false);
+            mPrevButton.setEnabled(false);
+            mQuestionText.setEnabled(false);
+            double result = CORRECT_ANSWERS*100/mQuestionBank.length;
+            Toast.makeText(MainActivity.this,"Your result: "+result+"%",Toast.LENGTH_LONG).show();
+        }
     }
 }
